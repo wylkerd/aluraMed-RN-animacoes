6e110d26eb82a4f9ea85656570e29f47e776c0cc
Wylkerd Santos Silva <wylkerd.silva@modalgr.com.br>
2023-01-16 10:17:20 -0300

    chore: criando o Shimmer Effect

---
+++ src/components/CardConsultaShimmerEffect/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import styles from "../CardConsulta/styles";
+import shimmerGif from '../../assets/shimmer.gif'; // Gif Shimmer effect retirado do site Lottie
+
+export function CardConsultaShimmerEffect() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.pessoaArea}>
+        <Image source={shimmerGif} style={[styles.foto, { overlayColor: '#FFF' }]} />
+        <View style={styles.informacoes}>
+          {/* nome */}
+          <Image source={shimmerGif} style={{ width: 200, height: 25 }} />
+          <View style={styles.consultaArea}>
+            {/* consulta */}
+            <Image source={shimmerGif} style={{ width: 100, height: 15, marginTop: 5 }} />
+          </View>
+        </View>
+      </View>
+      <View style={styles.informacoesAgendamento}>
+        {/* horário */}
+        <Image source={shimmerGif} style={{ width: 100, height: 15 }} />
+        <View style={styles.botoesArea}>
+          {/* botão */}
+          <Image source={shimmerGif} style={{ width: '100%', height: 50, borderRadius: 10, overlayColor: '#FFF' }} />
+        </View>
+      </View>
+    </View>
+  );
+}
--- src/screens/Principal/index.js
+++ src/screens/Principal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { InformacoesUsuario } from "../../components/InformacoesUsuario";
 import { CardConsulta } from "../../components/CardConsulta";
@@ -6,8 +6,16 @@ import { TelaDeFundo } from "../../components/TelaDeFundo";
 import soniaFoto from "../../assets/sonia.png";
 import pacientes from "./pacientes";
 import styles from "./styles";
+import { CardConsultaShimmerEffect } from "../../components/CardConsultaShimmerEffect";
 
 export default function Principal({ navigation }) {
+  const [tempo, setTempo] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTempo(true);
+    }, 3000)
+  }, [])
 
   return (
     <TelaDeFundo>
@@ -19,17 +27,25 @@ export default function Principal({ navigation }) {
       />
 
       <Text style={styles.texto}>Hoje</Text>
-
-      <FlatList
-        data={pacientes}
-        keyExtractor={item => String(item.id)}
-        renderItem={({ item }) => 
-        <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
-          <CardConsulta {...item} />
-        </TouchableOpacity>
-        }
-        showsVerticalScrollIndicator={false}
-      />
+      {
+        tempo ?
+        <FlatList
+          data={pacientes}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => 
+          <TouchableOpacity onPress={() => navigation.navigate("Detalhes", item)}>
+            <CardConsulta {...item} />
+          </TouchableOpacity>
+          }
+          showsVerticalScrollIndicator={false}
+        />
+        :
+        <>
+          <CardConsultaShimmerEffect />
+          <CardConsultaShimmerEffect />
+          <CardConsultaShimmerEffect />
+        </>
+      }
     </View> 
     </TelaDeFundo>
   );
