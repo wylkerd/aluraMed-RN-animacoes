9366cbb4e8edd2df8fa19fa38581afff93ff114c
Wylkerd Santos Silva <wylkerd.silva@modalgr.com.br>
2023-01-16 11:17:05 -0300

    chore: botão de notificacao, animação com react-native-reanimated

--- src/screens/Detalhes/index.js
+++ src/screens/Detalhes/index.js
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, TouchableOpacity, ScrollView, Button } from 'react-native';
 import { TelaDeFundo } from '../../components/TelaDeFundo';
 import { InformacoesUsuario } from '../../components/InformacoesUsuario';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import mapa from '../../assets/mapa.png';
 import styles from './styles';
-import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, withTiming, withRepeat } from 'react-native-reanimated';
 
 export default function Detalhes(props) {
   const dados = props.route.params;
 
-  // const posicao = useSharedValue(0);
+  const rotacao = useSharedValue(0);
+  const angulo = -30;
+  const [jaAnimou, setJaAnimou] = useState(false);
 
-  // const estiloAnimado = useAnimatedStyle(() => {
-  //   return {
-  //     transform: [
-  //       {
-  //         translateX: withSpring(posicao.value)
-  //       }
-  //     ]
-  //   }
-  // })
+  const estiloAnimado = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotate: `${rotacao.value}deg`
+        }
+      ]
+    }
+  })
 
-  // function alterarPosicaoBloco() {
-  //   posicao.value = Math.random() * 255;
-  // }
+  function fazerRotacao() {
+    rotacao.value = withRepeat(withTiming(angulo, { duration: 120 }), 6, true)
+
+    setTimeout(() => {
+      setJaAnimou(true);
+    }, 1000);
+  }
 
   return (
     <TelaDeFundo>
       <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-
-        {/* <Animated.View style={[{ backgroundColor: 'green', width: 50, height: 50 }, estiloAnimado]}/>
-        <Button title='Mova' onPress={alterarPosicaoBloco}/> */}
-
         <InformacoesUsuario
           nome={dados.nome}
           detalhes="Cliente desde 2018"
@@ -61,14 +63,17 @@ export default function Detalhes(props) {
           <Image style={styles.imagemMapa} source={mapa} />
           <Text>{dados.endereco}</Text>
           <TouchableOpacity 
-            style={styles.botao} 
+            style={styles.botao}
+            onPress={fazerRotacao} 
           >
             <Text style={styles.botaoTexto}>Notificar consulta</Text>
-              <Icon 
-                name={'notifications-none'} 
-                size={20} 
-                color="#FFF"
-              />
+              <Animated.View style={[ styles.icone, estiloAnimado ]}>
+                <Icon 
+                  name={ jaAnimou ? 'notifications' : 'notifications-none' } 
+                  size={20} 
+                  color="#FFF"
+                />
+              </Animated.View>
           </TouchableOpacity>
       </ScrollView>
     </TelaDeFundo>
