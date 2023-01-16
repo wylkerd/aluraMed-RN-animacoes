947e45394ad7a823725750ffddda9793957aa15c
Wylkerd Santos Silva <wylkerd.silva@modalgr.com.br>
2023-01-16 11:54:57 -0300

    chore: splashScreen com Lottie

--- src/rotas.js
+++ src/rotas.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Tab = createNativeStackNavigator();
 
+import SplashScreen from './screens/SplashScreen';
 import Onboarding from './screens/Onboarding';
 import Principal from './screens/Principal';
 import Detalhes from './screens/Detalhes';
@@ -12,6 +13,7 @@ export default function Rotas() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
+        <Tab.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }}/>
         <Tab.Screen name="Onboarding" component={Onboarding} options={{ headerShown: false }}/>
         <Tab.Screen name="Principal" component={Principal} options={{ headerShown: false }}/>
         <Tab.Screen name="Detalhes" component={Detalhes}/>
